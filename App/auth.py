acc51eabe290d72ba6009a4c36890c9fbee12d5c
LaurenzSoltau <laurenz.soltau@icloud.com>
2022-11-16 20:36:16 +0100

    register view

--- App/auth.py
+++ App/auth.py
@@ -9,3 +9,26 @@ from App.db import get_db
 
 # create blueprint named auth and the urls prefix auth
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+# view to register a new user
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    # if method is Post check inputs and load into database
+    if request.method == "POST":
+        # get the data from the form
+        username = request.form["usedrname"]
+        password = request.form["password"]
+        db = get_db() # load database
+        error = None
+
+        # guard clauses to validate inputs
+        if not username:
+            error = "Username required."
+        elif not password:
+            error = "Password required."
+
+        # if the input is valid call database and save the new user in users
+        if error is None:
+            pass
+
+
